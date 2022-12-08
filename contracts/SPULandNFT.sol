5e7d4327384b11591d8797a063509d6408d0e8bf
Gustavo <gustavo@obex.pw>
2022-12-08 13:21:48 -0300

    started NFT code

--- contracts/SPULandNFT.sol
+++ contracts/SPULandNFT.sol
@@ -1,8 +1,116 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.17;
 
-contract SPULandNFT {
-    constructor() {
-        
+import "erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./IERC4907.sol";
+
+contract SPULandNFT is
+    ERC721AUpgradeable,
+    Initializable,
+    AccessControlUpgradeable,
+    IERC4907
+{
+    struct UserInfo {
+        address user; // address of user role
+        uint64 expires; // unix timestamp, user expires
+    }
+
+    mapping(uint256 => UserInfo) internal _users;
+
+    uint256 public rip;
+
+    function initialize(
+        uint256 rip_,
+        uint8 fractions
+    ) external initializerERC721A initializer {
+        rip = rip_;
+        // maxMint = maxMint_;
+        // startDate = startDate_;
+        // endDate = endDate_;
+        // homepage = homepage_;
+
+        // _tokenURI = tokenURI_;
+
+        __ERC721A_init("SPULand", "SPUL");
+        __AccessControl_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _safeMint(_msgSender(), fractions);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(AccessControlUpgradeable, ERC721AUpgradeable)
+        returns (bool)
+    {
+        return
+            interfaceId == type(AccessControlUpgradeable).interfaceId ||
+            interfaceId == type(IERC4907).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    /// @notice set the user and expires of a NFT
+    /// @dev The zero address indicates there is no user
+    /// Throws if `tokenId` is not valid NFT
+    /// @param user  The new user of the NFT
+    /// @param expires  UNIX timestamp, The new user could use the NFT before expires
+    function setUser(
+        uint256 tokenId,
+        address user,
+        uint64 expires
+    ) public virtual {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        UserInfo storage info = _users[tokenId];
+        info.user = user;
+        info.expires = expires;
+        emit UpdateUser(tokenId, user, expires);
+    }
+
+    /// @notice Get the user address of an NFT
+    /// @dev The zero address indicates that there is no user or the user is expired
+    /// @param tokenId The NFT to get the user address for
+    /// @return The user address for this NFT
+    function userOf(uint256 tokenId) public view virtual returns (address) {
+        if (uint256(_users[tokenId].expires) >= block.timestamp) {
+            return _users[tokenId].user;
+        } else {
+            return address(0);
+        }
+    }
+
+    /// @notice Get the user expires of an NFT
+    /// @dev The zero value indicates that there is no user
+    /// @param tokenId The NFT to get the user expires for
+    /// @return The user expires for this NFT
+    function userExpires(
+        uint256 tokenId
+    ) public view virtual returns (uint256) {
+        return _users[tokenId].expires;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, tokenId);
+
+        if (from != to && _users[tokenId].user != address(0)) {
+            delete _users[tokenId];
+            emit UpdateUser(tokenId, address(0), 0);
+        }
     }
 }
