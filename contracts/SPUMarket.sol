582f75cccd3ec2192bf9d02d96760f013262bc5d
Gustavo <gustavo@obex.pw>
2022-12-08 10:18:39 -0300

    added openzeppelin and start function to create land

--- contracts/SPUMarket.sol
+++ contracts/SPUMarket.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.17;
 
-contract SPUMarket {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SPUMarket is Ownable {
+    
+    private uint256[] lands;
+
+
     constructor() {
         
     }
 
-    
+    function createLand(uint256 rip, uint8 fractions) onlyOwner {
+        
+    }
 }
